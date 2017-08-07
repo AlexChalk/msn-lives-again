e1bbfd764386701eb883d48817bfb322dde03a91
Alex Chalk <alexchalk17@gmail.com>
2017-08-07 14:55:33 -0400

    Add simplest possible landing page

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App.js';
+import './index.css';
+
+render(
+  <p>hello</p>,
+  // <App />,
+  document.getElementById('root')
+);
