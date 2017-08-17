dc1516f69ed1b4337478bf87dd09ffeaac10c3c4
Alex Chalk <alexchalk17@gmail.com>
2017-08-17 10:58:57 -0400

    Improve auto-contact-adding mechanism
    Relying on db validation to prevent contact duplication is ugly, so the
    saga that watches incoming messages now checks whether a contact number
    exists in app state before submitting an api request to add the contact.

--- client/src/sagas/receiveMessageSaga.js
+++ client/src/sagas/receiveMessageSaga.js
@@ -1,17 +1,15 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
-import axios from 'axios';
+import { put, select, takeEvery } from 'redux-saga/effects';
+import { getContacts } from './selectors.js';
 
 export default function* watchReceiveMessageAsync() {
-  yield takeEvery('RECEIVE_MESSAGE', autoAddContactAsync);
+  yield takeEvery('RECEIVE_MESSAGE', autoAddMissingContactAsync);
 }
 
-export function* autoAddContactAsync(action) {
-  try {
-    const response = yield call(axios.post, window.location.origin + 
-      '/contacts', {number: action.contact});
-    yield put({ type: 'ADD_CONTACT_SUCCESS', 
-      number: response.data.number});
-  } catch (error) {
-    yield put({ type: 'ADD_CONTACT_FAILURE', error: error});
+export function* autoAddMissingContactAsync(action) {
+  const contacts = yield select(getContacts);
+
+  if (!contacts.filter(c => c.number === action.contact)[0]) {
+    yield put({ type: 'ADD_CONTACT', number: action.contact});
   }
+
 }
