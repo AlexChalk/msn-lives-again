056694276c77c67b2938e9a80b7c7453857af4b7
Alex Chalk <alexchalk17@gmail.com>
2017-09-03 16:14:46 -0400

    Improve jsdom/mocha/enzyme boilerplate

--- client/test/setup.js
+++ client/test/setup.js
@@ -1,13 +1,20 @@
 require('babel-register')(); 
 
-const jsdom = require('jsdom');
-const doc = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
+const { JSDOM } = require('jsdom');
 
-global.document = doc;
-global.window = doc.window;
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
 
-Object.keys(window).forEach((key) => {
-  if (!(key in global)) {
-    global[key] = window[key];
-  }
-});
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+copyProps(window, global);
