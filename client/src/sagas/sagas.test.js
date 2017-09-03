ad6bc8c106f6af89cc1b48df1bfc1ef84a569bd8
Alex Chalk <alexchalk17@gmail.com>
2017-09-03 18:40:58 -0400

    Complete testing of first saga

--- client/src/sagas/sagas.test.js
+++ client/src/sagas/sagas.test.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import axios from 'axios';
 import { call, put } from 'redux-saga/effects';
 
 import { sendMessageAsync } from './sendMessageSaga.js';
@@ -7,17 +8,50 @@ import { addContactAsync } from './addContactSaga.js';
 import { loadDatabaseAsync } from './loadDatabaseSaga.js';
 
 describe('sagas', () => {
-  it('sendMessageAsync sends message asynchronously', () => {
-    console.log(window.location.href);
-    // const status = sendMessageAsync({
-    //   type: 'SEND_MESSAGE',
-    //   body: 'Hey',
-    //   contact: '+1234567890'
-    // });
-    // // console.log(status.next());
-    // expect(status.next().value).to.deep.equal(call(axios.post, window.location.origin + '/sms/outgoing', data);
 
-    
-    
+  describe('sendMessageAsync', () => {
+    const data = {
+      type: 'SEND_MESSAGE',
+      body: 'Hey',
+      contact: '+1234567890'
+    };
+
+    it('sendMessageAsync sends message asynchronously', () => {
+      const status = sendMessageAsync(data);
+
+      expect(status.next().value).to.deep.equal(
+        call(axios.post, window.location.origin + '/sms/outgoing', 
+          { body: 'Hey',
+            to: '+1234567890',
+            from: undefined }));
+
+      // Arg to status.next mocks api response    
+      expect(status.next({ data: { 
+        contact: '+1234567890',
+        messageSid: '1234ABCDE'} }).value).to.deep.equal(
+          put({
+            type: 'SEND_MESSAGE_SUCCESS', 
+            body: 'Hey',
+            contact: '+1234567890',
+            messageSid: '1234ABCDE',
+          }));
+
+      expect(status.next()).to.deep.equal({ 
+        done: true, 
+        value: undefined });
+    });
+
+    it('sendMessageAsync returns error message on failure', () => {
+      const status = sendMessageAsync(data);
+      status.next();
+
+      expect(status.throw('Missing message parameters').value)
+        .to.deep.equal(
+        put({
+          type: 'SEND_MESSAGE_FAILURE',
+          error: 'Missing message parameters'
+        }));
+    });
+
   });
 });
